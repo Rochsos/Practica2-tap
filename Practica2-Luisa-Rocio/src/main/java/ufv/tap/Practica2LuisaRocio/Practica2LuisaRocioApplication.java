5f751c9ce4b0dd509c1bc18a4d519d30289989ae
Rocio Sosa <rociioo1998@gmail.com>
2020-05-25 12:33:59 +0200

    Creando proyecto vacio maven con spring y vaadin

---
+++ Practica2-Luisa-Rocio/src/main/java/ufv/tap/Practica2LuisaRocio/Practica2LuisaRocioApplication.java
@@ -0,0 +1,13 @@
+package ufv.tap.Practica2LuisaRocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2LuisaRocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2LuisaRocioApplication.class, args);
+	}
+
+}
---
+++ Practica2-Luisa-Rocio/src/test/java/ufv/tap/Practica2LuisaRocio/Practica2LuisaRocioApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.tap.Practica2LuisaRocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2LuisaRocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
